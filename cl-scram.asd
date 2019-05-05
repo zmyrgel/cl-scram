d7d8496b729e68064d39a0279d7f0c8fffbd99fe
Timo Myyrä <timo.myyra@bittivirhe.fi>
2019-05-05 09:09:22 +0300

    add WIP unit tests for cl-scram using rove

--- cl-scram.asd
+++ cl-scram.asd
@@ -1,4 +1,3 @@
-;;;; System definition for cl-scram-sha1
 (in-package #:cl-user)
 (defpackage #:cl-scram-asd
   (:use #:cl #:asdf))
@@ -22,4 +21,20 @@
                                         (:file "utils")
                                         (:file "scram")))
                   (:static-file "README.md")
-                  (:static-file "LICENSE")))
+                  (:static-file "LICENSE"))
+  :in-order-to ((test-op (test-op "cl-scram/tests"))))
+
+(defsystem "cl-scram/tests"
+  :name "cl-scram/tests"
+  :version "0.1"
+  :author "Timo Myyrä <timo.myyra@bittivirhe.fi>"
+  :maintainer "Timo Myyrä <timo.myyra@bittivirhe.fi>"
+  :description "Unit tests for the cl-scram."
+  :license "Revised BSD License (see LICENSE)"
+  :depends-on ("cl-scram" "rove")
+  :serial t
+  :components ((:module "tests"
+                :components ((:file "scram"))))
+  :perform (test-op (op c)
+                    (funcall (read-from-string "rove:run")
+                             :cl-scram/tests :style :spec)))
---
+++ tests/scram.lisp
@@ -0,0 +1,48 @@
+(in-package :cl-user)
+(defpackage cl-scram.t.scram
+  (:use :cl :rove
+        :cl-scram))
+(in-package :cl-scram.t.scram)
+
+(deftest base64-api-tests
+    (testing "Verifying that public API functions work"
+             (ok (string= (base64-decode "Zm9vYmFy") "foobar")
+                 "base64 decoding should work for ASCII strings")
+             (ok (string= (base64-encode "foobar") "Zm9vYmFy")
+                 "base64 encoding should work for ASCII strings.")
+             (ok (string= (base64-encode-octets #(102 111 111 98 97 114)) "Zm9vYmFy")
+                 "base64 encoding should work on octets")))
+
+(deftest sasl-password-tests
+    (testing "Verify SASL password works"
+             (ok (string= (gen-sasl-password "foo") "foo"))))
+
+(deftest client-api-tests
+    (testing "Client API functions"
+             (ok (= (length (gen-client-nonce)) 32))
+             (ok (string= (gen-client-encoded-initial-message :username "foo"
+                                                              :nonce "bar")
+                          "biwsbj1mb28scj1iYXI="))
+             (ok (string= (gen-client-initial-message :username "foo"
+                                                      :nonce "bar")
+                          "n,,n=foo,r=bar"))
+             (ok (let ((nonce (gen-client-nonce)))
+                   (string= (gen-client-final-message :password "foobar"
+                                                      :client-nonce nonce
+                                                      :client-initial-message "foo"
+                                                      :server-response "foo")
+                            "foo")))
+             ))
+
+(deftest parsing-tests
+  (testing "Server parsing function tests"
+    (let ((resp "r=fyko+d2lbbFgONRv9qkxdawL3rfcNHYJY1ZVvWVs7j,s=QSXCR+Q6sek8bf92,i=4096"))
+      (ok (tree-equal (parse-server-nonce :response resp)
+                      '(("r" . "fyko+d2lbbFgONRv9qkxdawL3rfcNHYJY1ZVvWVs7j")
+                        ("s" . "QSXCR+Q6sek8bf92")
+                        ("i" . "4096"))
+                      :test 'equalp)))
+             (ok (string= (parse-server-salt :response resp)
+                          "A%ÂGä:±é<mÿv"))
+             (ok (= (parse-server-iterations :response resp)
+                    4096))))
